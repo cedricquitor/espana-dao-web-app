f6eca8502ee02a03a94505335e903c8731728310
cedricquitor <cedricquitor.dev@gmail.com>
2022-04-15 22:41:38 +0800

    Revoked the roles for the governance token

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { useAddress, useMetamask, useEditionDrop, useToken, useVote } from "@thirdweb-dev/react";
+import { useAddress, useMetamask, useEditionDrop, useToken, useVote, useNetwork } from "@thirdweb-dev/react";
 import { useState, useEffect, useMemo } from "react";
+import { ChainId } from "@thirdweb-dev/sdk";
 import { AddressZero } from "@ethersproject/constants";
 
 const App = () => {
   const address = useAddress();
+  const network = useNetwork();
   const connectWithMetamask = useMetamask();
   console.log("👋 Address:", address);
 
@@ -169,6 +171,15 @@ const App = () => {
       setIsClaiming(false);
     }
   };
+
+  if (network?.[0].data.chain.id !== ChainId.Rinkeby) {
+    return (
+      <div className="unsupported-network">
+        <h2>Please connect to Rinkeby</h2>
+        <p>This dapp only works on the Rinkeby network, please switch networks in your connected wallet.</p>
+      </div>
+    );
+  }
 
   // This is the case where the user hasn't connected their wallet
   // to your web app. Let them call connectWallet.
